1d85c063d977182d0613d486cd71cacb14de7c1b
dthongkhanh <khanhdth_intern@flydinotech.com>
2024-06-05 09:03:27 +0700

    Add ui sidebar

---
+++ src/components/molecules/MenuStory.vue
@@ -0,0 +1,34 @@
+<template>
+  <el-card class="m-[20px]">
+    <template #header>
+      <div class="card-header text-main-color">
+        <span>{{ title }}</span>
+      </div>
+    </template>
+    <el-menu-item
+      :index="indexPrefix + story.id"
+      v-for="story in stories"
+      :key="indexPrefix + story.id"
+      :title="story.title"
+    >
+      <div :class="['justify-between', 'w-full', 'flex', 'items-center']">
+        <span class="overflow-hidden text-ellipsis">{{ story.title }}</span>
+        <span v-if="story.views" class="text-gray-500">
+          <el-icon><View /></el-icon> {{ story.views }}
+        </span>
+      </div>
+    </el-menu-item>
+  </el-card>
+</template>
+
+<script setup lang="ts">
+import type { Story } from '@/types'
+import { View } from '@element-plus/icons-vue'
+
+defineProps<{
+  title: string
+  indexPrefix: string
+  stories: Story[]
+}>()
+</script>
+<style scoped></style>
--- src/components/organisms/DefaultList.vue
+++ src/components/organisms/DefaultList.vue
@@ -1,3 +1,54 @@
-<template>list</template>
+<template>
+  <el-menu class="!bg-inherit !border-none" active-text-color="#1ed291">
+    <MenuGenre title="Truyện tranh" index-prefix="comic-" :genres="comicGenres"></MenuGenre>
+    <MenuGenre title="Truyện chữ" index-prefix="novel-" :genres="novelGenres"></MenuGenre>
+    <MenuStory
+      title="Truyện được xem nhiều nhất"
+      index-prefix="popular-"
+      :stories="popularStories"
+    ></MenuStory>
+    <MenuStory title="Truyện mới phát hành" index-prefix="new-" :stories="newStories"></MenuStory>
+  </el-menu>
+</template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type { Genre } from '@/types'
+
+const genres = ref<Genre[]>([
+  { id: 1, name_genre: 'Khoa học' },
+  { id: 2, name_genre: 'Lịch sử' },
+  { id: 3, name_genre: 'Cổ đại' }
+])
+
+const stories = ref([
+  { id: 1, title: 'Truyện Khoa học 1', id_genre: 1, type: 'comic' },
+  { id: 2, title: 'Truyện Lịch sử 1', id_genre: 2, type: 'comic' },
+  { id: 3, title: 'Truyện Cổ đại 1', id_genre: 3, type: 'novel' }
+])
+
+const comicGenres = computed(() =>
+  genres.value.filter((genre) =>
+    stories.value.some((story) => story.id_genre === genre.id && story.type === 'comic')
+  )
+)
+
+const novelGenres = computed(() =>
+  genres.value.filter((genre) =>
+    stories.value.some((story) => story.id_genre === genre.id && story.type === 'novel')
+  )
+)
+
+const popularStories = ref([
+  { id: 1, title: 'Truyện Khoa học 1', views: 123 },
+  {
+    id: 2,
+    title: 'Truyện Cổ đại 1',
+    views: 186
+  }
+])
+
+const newStories = ref([
+  { id: 1, title: 'Truyện Cổ đại 1' },
+  { id: 2, title: 'Truyện Lịch sử 1' }
+])
+</script>
